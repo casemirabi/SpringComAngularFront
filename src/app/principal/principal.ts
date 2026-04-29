7e835137705f6e6e1635759cfc6d04905ad8d875
casemirabi <biancacaasemira@gmail.com>
2026-04-29 15:43:46 -0300

    Log dos valores quando há um evento cadastrar

--- src/app/principal/principal.ts
+++ src/app/principal/principal.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-principal',
-  imports: [],
+  imports: [
+    // FormsModule permite usar [(ngModel)] no HTML
+    FormsModule
+  ],
   templateUrl: './principal.html',
   styleUrl: './principal.css',
+
 })
 export class Principal {
   //  Variavel para visibilidade de botoes
   btnCadastro: boolean = true;
+
+  // Model: objeto que representa os dados do formulário
+  cliente = {
+    nome: '',
+    idade: null as number | null,
+    cidade: ''
+  };
+
+   // Controller: função chamada ao clicar no botão
+   cadastrar(): void{
+    // Exibe o objeto completo no console
+    console.log('Cliente cadastrado: ', this.cliente);
+   }
+
 }
 
 
